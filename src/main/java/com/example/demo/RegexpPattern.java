2fbf6bc2227e181b987711b73800273d0626a541
odagiritomoya <OdagiriTomoya@outlook.jp>
2021-02-09 18:18:20 +0900

    更新と削除部分（resultパッケージ）のControl実装完了

--- src/main/java/com/example/demo/RegexpPattern.java
+++ src/main/java/com/example/demo/RegexpPattern.java
@@ -8,6 +8,7 @@ public class RegexpPattern {
 	static final public String EMPTY = "^$";
 	static final public String RATE = "^[0-1]|(0\\.[%d]+)$";
 	static final public String ID_OR_NAME = "^(([%d]+)|([.]*))?$";
+	static final public String ID_OR_ID_AND_NAME = "^(([%d]+)|([%d]+:[.]+))$";
 	static final public String DATE = "^([1-2][0-9]{3})////(0?[1-9]|1[0-2])////(0?[1-9]|[12][0-9]|3[01])$";
 	static final public String RANGE = "^([.]*)-([.]*)$";
 	static final public String RANGE_DATE = "^((" + DATE + ")-(" + DATE + "))|(" + DATE + ")?$";
